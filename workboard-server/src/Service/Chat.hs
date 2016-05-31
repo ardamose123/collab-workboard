44b75c4f7d421099613871ffbb03cf52fcb86aa8
Ariel D. Moya Sequeira <ardamose123@gmail.com>
2016-05-30 23:53:23 -0600

    First commit, with all existing files in the project.

---
+++ workboard-server/src/Service/Chat.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE TypeOperators  #-}
+
+module Service.Chat where
+
+import qualified Data.ByteString.Lazy as BS
+
+import Control.Applicative
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TMVar
+import Control.Concurrent.STM.TChan
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
+import System.AtomicWrite.Writer.LazyByteString
+import System.Directory
+import System.FilePath
+import Text.Printf
+import Data.Word
+import Data.Aeson
+
+
+type SerialId = Word64
+
+data ChatAction message
+  = Say    message (TMVar SerialId)
+  | Listen Word64  (TMVar (Maybe message))
+
+type Chat a = TChan (ChatAction a)
+
+
+newChat :: IO (Chat message)
+newChat = newTChanIO
+
+
+sparkChatProcessor :: (FromJSON message, ToJSON message)
+  => FilePath
+  -> Chat message
+  -> IO ThreadId
+sparkChatProcessor root queue = do
+  startId  <- (fromIntegral . subtract 2 . length) <$> getDirectoryContents root
+  forkIO $ evalStateT (forever processChat) startId
+
+  where
+    processChat :: StateT SerialId IO ()
+    processChat = liftIO (atomically $ readTChan queue) >>= processChatAction
+
+    processChatAction :: (ToJSON a, FromJSON a) => ChatAction a -> StateT SerialId IO ()
+    processChatAction (Say message tmvar) = do
+      serialId <- get
+      liftIO $ do
+        atomicWriteFile (root </> printf "%020u" serialId) (encode message)
+        atomically $ putTMVar tmvar serialId
+      put (succ serialId)
+    processChatAction (Listen i tmvar) = liftIO $ do
+      contents <- liftIO $ BS.readFile (root </> printf "%020u" i)
+      atomically (putTMVar tmvar (decode contents))
+
+
+listen :: Chat a -> SerialId -> IO (Maybe a)
+listen stream serialId = do
+  responseBox <- newEmptyTMVarIO
+  atomically $ writeTChan stream (Listen serialId responseBox)
+  atomically $ takeTMVar responseBox
+
+
+say :: Chat a -> a -> IO SerialId
+say stream bytes = do
+  responseBox <- newEmptyTMVarIO
+  atomically $ writeTChan stream (Say bytes responseBox)
+  atomically $ takeTMVar responseBox
---
+++ workboard-server/src/Service/Chat/StateAggegator.hs
@@ -0,0 +1,40 @@
+module Service.Chat.StateAggregator(sparkChatProcessor) where
+
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
+import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.State
+import Data.Aeson
+import Data.Maybe
+import Data.Word
+import Service.Chat
+
+
+-- | Runs a new state updater thread that monitors a given chat.
+sparkChatStateUpdater :: FromJSON a
+  => (a -> s -> s)  -- ^ A function to derive a new state from an old state and a chat message.
+  -> s              -- ^ The start state.
+  -> Chat a         -- ^ The chat to monitor.
+  -> IO (TVar s)    -- ^ A variable to retrieve the updated state when needed.
+sparkChatStateUpdater f start chat = do
+  (stateVar, chat') <- atomically ((,) <$> newTVar start <*> dupTChan chat)
+  forkIO $ evalStateT (loop f stateVar chat') 0
+  pure stateVar
+
+
+-- | The loop of the state updater thread.
+loop :: FromJSON a
+  => (a -> s -> s)  -- ^ A function to derive a new state from an old state and a chat message.
+  -> TVar s         -- ^ A variable with the current state.
+  -> Chat a         -- ^ The chat to monitor.
+  -> StateT SerialId IO () -- ^ A state monad that holds the latest-read serial.
+loop merge stateVar chat = forever $ do
+  i <- get
+  msg <- liftIO (listen chat i)
+  when (isJust msg) $ do
+    oldState <- liftIO . atomically $ readTVar stateVar
+    let newState = merge (fromJust msg) oldState
+    liftIO . atomically $ writeTVar stateVar newState
+    put (succ i)
